76b3537df0dfb45d2768b4fff68911c774e4926f
janelikurm <janelikurm@gmail.com>
2023-03-23 14:18:37 +0200

    Session implementation WIP

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,17 +1,24 @@
 import java.util.Random;
 
 public class Game {
-    private Integer randomNumber;
-    private boolean gameStatus;
-    private int gamesWonCounter = 0;
-    private int gamesEndedWithoutWinCounter = 0;
-    private int currentGameGuessCounter = 0;
-    private int totalGuessCounter = 0;
-    private int averageGuessesToWinGameCounter = 0;
+    public Integer randomNumber;
+    public boolean gameStatus;
+    public int gamesWonCounter = 0;
+    public int gamesEndedWithoutWinCounter = 0;
+    public int currentGameGuessCounter = 0;
+    public int totalGuessCounter = 0;
+    public int averageGuessesToWinGameCounter = 0;
 
 
     public Game() {
-        this.randomNumber = new Random().nextInt(1, 101);;
+        this.randomNumber = new Random().nextInt(1, 101);
+        ;
+        this.gameStatus = true;
+    }
+
+    public void newGame() {
+        this.randomNumber = new Random().nextInt(1, 101);
+        ;
         this.gameStatus = true;
     }
 }
--- src/main/java/RequestHandler.java
+++ src/main/java/RequestHandler.java
@@ -1,15 +1,14 @@
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.Request;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Random;
 
 class RequestHandler implements HttpHandler {
 
@@ -20,15 +19,6 @@ class RequestHandler implements HttpHandler {
     Map<Integer, Game> sessions = new HashMap<>();
     Integer sessionNumber = 0;
 
-    private Integer randomNumber;
-    private boolean gameStatus;
-    private int gamesWonCounter = 0;
-    private int gamesEndedWithoutWinCounter = 0;
-    private int currentGameGuessCounter = 0;
-    private int totalGuessCounter = 0;
-    private int averageGuessesToWinGameCounter = 0;
-
-
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         String requestBody = new String(exchange.getRequestBody().readAllBytes(), StandardCharsets.UTF_8);
@@ -36,16 +26,16 @@ class RequestHandler implements HttpHandler {
         String methodAndPath = exchange.getRequestMethod() + " " + exchange.getRequestURI().getPath();
 
         int sessionKey = 0;
-        if (!exchange.getRequestURI().getQuery().isEmpty())
-            sessionKey = Integer.parseInt(exchange.getRequestURI().getQuery());
-
+        if (!(exchange.getRequestURI().getQuery() == null)) {
+            sessionKey = Integer.parseInt(exchange.getRequestURI().getQuery().split("=")[1]);
+        }
         try {
             switch (methodAndPath) {
-                case "GET /status" -> response = handleStatus();
-                case "POST /start-game" -> response = handleStart();
-                case "POST /guess" -> response = handleGuess(requestBody);
-                case "POST /end-game" -> response = handleStop();
-                case "GET /stats" -> response = handleStatistics();
+                case "GET /status" -> response = handleStatus(sessionKey);
+                case "POST /start-game" -> response = handleStart(sessionKey);
+                case "POST /guess" -> response = handleGuess(requestBody, sessionKey);
+                case "POST /end-game" -> response = handleStop(sessionKey);
+                case "GET /stats" -> response = handleStatistics(sessionKey);
                 default -> handleNotFound(exchange);
             }
         } catch (IOException e) {
@@ -59,47 +49,50 @@ class RequestHandler implements HttpHandler {
         log(exchange);
     }
 
-    private Response handleStatistics() {
-        String responseBody = "Games won: " + gamesWonCounter + "\n" +
-                "Games lost: " + gamesEndedWithoutWinCounter + "\n" +
-                "Average guesses per game: " + averageGuessesToWinGameCounter + "\n" +
-                "Current game guess count: " + currentGameGuessCounter + "\n" +
-                "Total guesses: " + totalGuessCounter;
+    private Response handleStatistics(int sessionKey) {
+        String responseBody = "Games won: " + sessions.get(sessionKey).gamesWonCounter + "\n" +
+                "Games lost: " + sessions.get(sessionKey).gamesEndedWithoutWinCounter + "\n" +
+                "Average guesses per game: " + sessions.get(sessionKey).averageGuessesToWinGameCounter + "\n" +
+                "Current game guess count: " + sessions.get(sessionKey).currentGameGuessCounter + "\n" +
+                "Total guesses: " + sessions.get(sessionKey).totalGuessCounter;
         return new Response(200, responseBody);
     }
 
-    private Response handleStatus() {
-        return gameStatus ? new Response(200, "true") : new Response(200, "false");
+    private Response handleStatus(int sessionKey) {
+        return new Response(200, "" + sessions.get(sessionKey).gameStatus);
     }
 
-    private Response handleStart() {
-        if (!gameStatus) {
+    private Response handleStart(int sessionKey) {
+        if (sessionKey == 0) {
             Game game = new Game();
             sessionNumber++;
             sessions.put(sessionNumber, game);
             return new Response(200, "GameStatus > Started. Session:" + sessionNumber);
+        } else if (!sessions.get(sessionKey).gameStatus) {
+            sessions.get(sessionKey).newGame();
+            return new Response(200, "GameStatus > Started. Session:" + sessionKey);
         } else {
             return new Response(400, "Game already running.");
         }
     }
 
-    private Response handleGuess(String requestBody) {
+    private Response handleGuess(String requestBody, int sessionKey) {
 
-        if (!gameStatus) return new Response(400, "Game is not active.");
+        if (!sessions.get(sessionKey).gameStatus) return new Response(400, "Game is not active.");
         try {
-            return numberControl(Integer.parseInt(requestBody));
+            return numberControl(Integer.parseInt(requestBody), sessionKey);
         } catch (NumberFormatException e) {
             return new Response(400, "Invalid input.");
         }
     }
 
-    private Response handleStop() {
+    private Response handleStop(int sessionKey) {
 
-        if (gameStatus) {
-            randomNumber = null;
-            gameStatus = false;
-            gamesEndedWithoutWinCounter = gamesEndedWithoutWinCounter + 1;
-            currentGameGuessCounter = 0;
+        if (sessions.get(sessionKey).gameStatus) {
+            sessions.get(sessionKey).randomNumber = null;
+            sessions.get(sessionKey).gameStatus = false;
+            sessions.get(sessionKey).gamesEndedWithoutWinCounter = sessions.get(sessionKey).gamesEndedWithoutWinCounter + 1;
+            sessions.get(sessionKey).currentGameGuessCounter = 0;
             return new Response(200);
         } else {
             return new Response(400, "Game is not active.");
@@ -125,25 +118,25 @@ class RequestHandler implements HttpHandler {
         }
     }
 
-    private Response numberControl(int userGuess) {
+    private Response numberControl(int userGuess, int sessionKey) {
         Response response = new Response(200);
-        if (userGuess <= 0 || userGuess > 100 || !gameStatus) {
+        if (userGuess <= 0 || userGuess > 100 || !sessions.get(sessionKey).gameStatus) {
             response.responseBody = "Number out of bounds!";
             response.statusCode = 400;
-        } else if (randomNumber == userGuess) {
-            gameStatus = false;
-            randomNumber = null;
-            gamesWonCounter += 1;
-            totalGuessCounter += currentGameGuessCounter;
-            currentGameGuessCounter = 0;
-            averageGuessesToWinGameCounter = totalGuessCounter / gamesWonCounter;
+        } else if (sessions.get(sessionKey).randomNumber == userGuess) {
+            sessions.get(sessionKey).gameStatus = false;
+            sessions.get(sessionKey).randomNumber = null;
+            sessions.get(sessionKey).gamesWonCounter += 1;
+            sessions.get(sessionKey).totalGuessCounter += sessions.get(sessionKey).currentGameGuessCounter;
+            sessions.get(sessionKey).currentGameGuessCounter = 0;
+            sessions.get(sessionKey).averageGuessesToWinGameCounter = sessions.get(sessionKey).totalGuessCounter / sessions.get(sessionKey).gamesWonCounter;
             response.responseBody = String.valueOf(NumberGuess.EQUAL);
-        } else if (userGuess < randomNumber) {
+        } else if (userGuess < sessions.get(sessionKey).randomNumber) {
             response.responseBody = String.valueOf(NumberGuess.LESS);
-            currentGameGuessCounter += 1;
+            sessions.get(sessionKey).currentGameGuessCounter += 1;
         } else {
             response.responseBody = String.valueOf(NumberGuess.BIGGER);
-            currentGameGuessCounter += 1;
+            sessions.get(sessionKey).currentGameGuessCounter += 1;
         }
         return response;
     }
