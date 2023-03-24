594b62ab5938204fe84c328d0ab73571a5ba945b
janelikurm <janelikurm@gmail.com>
2023-03-24 11:36:51 +0200

    Implemented game emulator test. Tested against 10.10.10.80 server.

--- src/test/java/GameServerTest.java
+++ src/test/java/GameServerTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -16,6 +17,7 @@ class GameServerTest {
 
     @Test
     void startGame() throws IOException, InterruptedException {
+        postRequest("end-game", "");
         assertEquals(200, postRequest("start-game", "").statusCode());
         assertEquals(400, postRequest("start-game", "").statusCode());
     }
@@ -27,25 +29,62 @@ class GameServerTest {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"", "  ", "randomtext", "250", "-250", "-2147483649", "2147483648"})
-    void guessBadInputs(String input) throws IOException, InterruptedException {
+    @ValueSource(strings = {"50", "", "  ", "0", "101", "-1", "-100", "randomtext", "250", "-250", "-2147483649", "2147483648"})
+    void endGameGuess(String input) throws IOException, InterruptedException {
+        postRequest("end-game", "");
+        assertEquals(400, postRequest("guess", input).statusCode());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "  ", "0", "101", "-1", "-100", "randomtext", "250", "-250", "-2147483649", "2147483648"})
+    void badInput(String input) throws IOException, InterruptedException {
         postRequest("start-game", "");
         assertEquals(400, postRequest("guess", input).statusCode());
         postRequest("end-game", "");
     }
-    @Test
-    void validInputEndedGame() throws IOException, InterruptedException {
-        postRequest("end-game", "");
-        assertEquals(400, postRequest("guess", "50").statusCode());
-    }
 
     @Test
-    void guessValidInputs() throws IOException, InterruptedException {
+    void validGuess() throws IOException, InterruptedException {
         for (int i = 1; i <= 100; i++) {
             postRequest("start-game", "");
             assertEquals(200, postRequest("guess", String.valueOf(i)).statusCode());
             postRequest("end-game", "");
         }
+    }
+
+    @RepeatedTest(5)
+    void gameWin() throws IOException, InterruptedException {
+        postRequest("end-game", "");
+        postRequest("start-game", "");
+        HttpResponse<String> gameResponse = aiGamerWins();
+        assertEquals("EQUAL", gameResponse.body());
+    }
+
+    @Test
+    void afterGameWin() throws IOException, InterruptedException {
+        postRequest("end-game", "");
+        postRequest("start-game", "");
+        aiGamerWins();
+        assertEquals(400, postRequest("guess", "50").statusCode());
+        assertEquals(400, postRequest("end-game", "").statusCode());
+        assertEquals(200, postRequest("start-game", "").statusCode());
+        assertEquals(400, postRequest("start-game", "").statusCode());
+        assertEquals(200, postRequest("end-game", "").statusCode());
+    }
+
+    private static HttpResponse<String> aiGamerWins() throws IOException, InterruptedException {
+        int guess = 50, max = 100, min = 1;
+        HttpResponse<String> gameResponse = postRequest("guess", String.valueOf(guess));
+        while (!"EQUAL".equals(gameResponse.body())) {
+            if ("BIGGER".equals(gameResponse.body())) {
+                max = guess;
+            } else {
+                min = guess;
+            }
+            guess = (max - min) / 2 + min;
+            gameResponse = postRequest("guess", String.valueOf(guess));
+        }
+        return gameResponse;
     }
 
     private static HttpResponse<String> getRequest(String uri) throws IOException, InterruptedException {
